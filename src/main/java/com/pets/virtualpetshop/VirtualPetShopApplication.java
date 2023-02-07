c3ca0561e7a9c2368104ac6e059a3660ee61f870
selimkucukkaraca <selimkkkaraca@hotmail.com>
2023-02-07 21:27:16 +0300

    Initial commit

---
+++ src/main/java/com/pets/virtualpetshop/VirtualPetShopApplication.java
@@ -0,0 +1,13 @@
+package com.pets.virtualpetshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualPetShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VirtualPetShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pets/virtualpetshop/VirtualPetShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pets.virtualpetshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualPetShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
